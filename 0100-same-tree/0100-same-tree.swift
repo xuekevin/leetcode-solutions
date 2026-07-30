d0efe6177d7f734fdbbbec2f7fb7285cfaef2c18
xuekevin <xuekaiwen.kevin@gmail.com>
2026-07-29 19:14:14 -0700

    Time: 0 ms (100%), Space: 19.5 MB (56.9%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.swift
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil && q == nil {
+            return true
+        }
+        guard let p = p else {
+            return false
+        }
+
+        guard let q = q else {
+            return false
+        }
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    }
+}
+
+// got crash for 2 mins writing
+// checking now
+// originally, just write return p?.val == q?.val && isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+// think can upwrap first to avoide nil access
+// update, try again
